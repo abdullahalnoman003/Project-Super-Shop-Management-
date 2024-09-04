1791f2116153813bcc438753f47478447ae4d1f9
Abdullah Al Noman <141672697+abdullahalnoman003@users.noreply.github.com>
2024-09-04 21:43:39 +0600

    Create project.c
    1st Commit. Welcome massege

---
+++ project.c
@@ -0,0 +1,32 @@
+/*
+  Super Shop Management System
+
+  Name: Abdullah Al Noman
+  ID: 232-35-003
+  
+  Name: Adnan Iqbal
+  ID: 232-35-024
+  
+  Name: Rashedul Islam
+  ID: 232-35-066*/
+
+
+#include<stdio.h>
+int main(){
+    //code starts here with Welcome message.
+    printf("**********************************************************************\n");
+    printf("**********************************************************************\n");
+    printf("***                                                                ***\n");
+    printf("*****                                                            *****\n");
+    printf("*******        Welcome to Super Shop Management System!        *******\n");
+    printf("*****                                                            *****\n");
+    printf("***                                                                ***\n");
+    printf("**********************************************************************\n");
+    printf("**********************************************************************\n");
+    printf("\n\n\n");
+
+
+
+    return 0;
+}
+
